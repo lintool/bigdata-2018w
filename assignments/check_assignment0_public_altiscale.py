38a53efd4686ba7ce0eb1333412cc38c4f2f2216
lintool <jimmylin@uwaterloo.ca>
2018-01-06 10:57:49 -0500

    More tweaks for a0 public check scripts.

--- assignments/check_assignment0_public_altiscale.py
+++ assignments/check_assignment0_public_altiscale.py
@@ -13,14 +13,14 @@ from subprocess import call
 import re
 
 def check_a0(u):
-  """Run assignment0 in linux environment"""
+  """Run Assignment 0 on Altiscale"""
   call(["mvn", "clean", "package"])
   call(["hadoop", "jar", "target/assignments-1.0.jar",
         "ca.uwaterloo.cs451.a0.PerfectX",
         "-input", "/shared/uwaterloo/cs451/data/enwiki-20161220-sentences-0.1sample.txt",
         "-output", "cs451-"+u+"-a0-wiki" ])
   print("Question 5.")
-  call("hadoop fs -cat cs451-"+u+"-a0-wiki/part* | sort -k 2 -n -r | head", shell=True)
+  call("hadoop fs -cat cs451-"+u+"-a0-wiki/part* | sort -k 2 -n -r | head -10", shell=True)
 
 if __name__ == "__main__":
   try:
--- assignments/check_assignment0_public_linux.py
+++ assignments/check_assignment0_public_linux.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """
 CS 451 Data-Intensive Distributed Computing (Winter 2018):
-Assignment 0 public check script
+Assignment 0 public check script for the Linux Student CS environment
 
 Sample usage:
 $ ./check_assignment0_public_linux.py lintool
@@ -13,14 +13,14 @@ from subprocess import call
 import re
 
 def check_a0(u):
-  """Run assignment0 in linux environment"""
+  """Run Assignment 0 in the Linux Student CS environment"""
   call(["mvn", "clean", "package"])
   call(["hadoop", "jar", "target/assignments-1.0.jar",
         "ca.uwaterloo.cs451.a0.PerfectX",
         "-input", "data/Shakespeare.txt",
         "-output", "cs451-"+u+"-a0-shakespeare" ])
   print("Question 1.")
-  call("hadoop fs -cat cs451-"+u+"-a0-shakespeare/part-r-00000 | sort -k 2 -n -r | head", shell=True)
+  call("hadoop fs -cat cs451-"+u+"-a0-shakespeare/part-r-00000 | sort -k 2 -n -r | head -1", shell=True)
 
 if __name__ == "__main__":
   try:
