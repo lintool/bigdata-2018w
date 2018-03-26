6da81a62a18fe648ae7a330bc26ffaa6b2c375e1
lintool <jimmylin@uwaterloo.ca>
2018-03-25 20:30:25 -0400

    Added mark breakdown and link to check script for A7.

--- assignments/check_assignment7_public.py
+++ assignments/check_assignment7_public.py
@@ -13,7 +13,7 @@ from subprocess import call
 import argparse
 import re
 
-def check_a7(memory):
+def check_a7(username, memory):
   spark_cmd = "spark-submit"
   data_prefix = "/u3/cs451/public_html/taxi-data"
 
@@ -24,25 +24,26 @@ def check_a7(memory):
          "--class", "ca.uwaterloo.cs451.a7.EventCount",
          "target/assignments-1.0.jar", "--input", "{0}".format(data_prefix),
          "--checkpoint", "cs451-lintool-checkpoint-q{}".format(1),
-         "--output", "output_{}/cs451-lintool-output-q{}".format(1,1)])
+         "--output", "output_{}/cs451-{}-output-q{}".format(1, username, 1)])
   print("2. RegionalEventCount")
   call([spark_cmd,"--driver-memory",memory,
          "--class", "ca.uwaterloo.cs451.a7.RegionEventCount",
-         "target/assignments-1.0.jar", "--input", "{0}".format(data_prefix),                      
-         "--checkpoint", "cs451-lintool-checkpoint-q{}".format(2), 
-         "--output", "output_{}/cs451-lintool-output-q{}".format(2, 2)])
+         "target/assignments-1.0.jar", "--input", "{0}".format(data_prefix),
+         "--checkpoint", "cs451-lintool-checkpoint-q{}".format(2),
+         "--output", "output_{}/cs451-{}-output-q{}".format(2, username, 2)])
   print("3. TrendingQuery")
   call([spark_cmd,"--driver-memory",memory,
-         "--class", "ca.uwaterloo.cs451.a7.TrendingQuery",
-         "target/assignments-1.0.jar", "--input", "{0}".format(data_prefix),                       
+         "--class", "ca.uwaterloo.cs451.a7.TrendingArrivals",
+         "target/assignments-1.0.jar", "--input", "{0}".format(data_prefix),
          "--checkpoint", "cs451-lintool-checkpoint-q{}".format(3),
-         "--output", "output_{}/cs451-lintool-output-q{}".format(3, 3)])
+         "--output", "output_{}/cs451-{}-output-q{}".format(3, username, 3)])
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="CS 451 Winter 2018 Assignment7 Public Test Script")
+  parser.add_argument('username',metavar='Github Username', help="Github username",type=str)
   parser.add_argument('-m','--memory',help="Amount of memory to give Spark jobs",type=str,default="2G")
   args=parser.parse_args()
   try:
-    check_a7(args.memory)
+    check_a7(args.username, args.memory)
   except Exception as e:
     print(e)
